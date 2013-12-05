a0bf21ecd3149111f66cbf4de875d1f550dc023f
Jason Hejna <jason.hejna@gmail.com>
2013-12-05 09:36:46 -0500

    session

--- grr/index.php
+++ grr/index.php
@@ -3,14 +3,24 @@ date_default_timezone_set('America/Detroit');
 
 $one = htmlspecialchars($_GET['vone']);
 $two = htmlspecialchars($_GET['vtwo']);
-if($one === "userpassword" && $two === "userpassword"){
+if($one === "4512" && $two === "trebuchet271ii45"){
 	$date = date('Y:m:d:H:i:s');
-	$db = new PDO('mysql:host=localhost;dbname=dbname', "dbuser", "dbpassword");
+	$db = new PDO('mysql:host=localhost;dbname=howmany1_coffeeconsumption', "howmany1_coffee", "a2FT^my=$+{p");
 	$stmt = $db->prepare("INSERT INTO cups (time) VALUES (:time)");
 	$stmt->execute(array(':time' => $date));
 	echo "plusone";
+	session_start();
+	$_SESSION['login']=$one;
+	echo $_SESSION['login'];
 }
 else{
 	echo "you failed again";
+}
+function addOne(){
+	$date = date('Y:m:d:H:i:s');
+	$db = new PDO('mysql:host=localhost;dbname=howmany1_coffeeconsumption', "howmany1_coffee", "a2FT^my=$+{p");
+	$stmt = $db->prepare("INSERT INTO cups (time) VALUES (:time)");
+	$stmt->execute(array(':time' => $date));
+	echo "plusone";
 }
 ?>

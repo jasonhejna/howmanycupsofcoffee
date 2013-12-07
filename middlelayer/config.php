643d9db6e41019cfe89cb723ee60c3f55c1c8a65
Jason Hejna <jason.hejna@gmail.com>
2013-12-07 04:20:46 -0500

    update

--- middlelayer/config.php
+++ middlelayer/config.php
@@ -1,11 +1,11 @@
 <?php
-//Please set a hard to crack $web_password, and a $web_username greater than four characters. I can't save you from yourself
+//Please use a difficult to crack password for $web_password, and a $web_username greater than four characters. I can't save you from yourself
 //Also, be aware, you should have ssl on the web server your running this on. Or else you're vulnerable to session injection attacks.
-$web_secret		= 'vone';
-$webusername	= '4512';
-$webpassword	= 'trebuchet271ii45';
+$web_secret		= '1234';
+$webusername	= 'myusername';
+$webpassword	= 'mypassword';
 $dbhostaddress = 'localhost';
-$dbname 		= 'howmany1_coffeeconsumption';
-$dbuser		= 'howmany1_coffee';
-$dbpass		= 'a2FT^my=$+{p';
+$dbname 		= 'mydbname';
+$dbuser		= 'mydbusername';
+$dbpass		= 'mydbpassword';
 ?>

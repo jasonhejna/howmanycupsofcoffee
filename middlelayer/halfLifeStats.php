89412d8d37790fec3e870f186261fff4eaee4278
Jason Hejna <jason.hejna@gmail.com>
2013-12-23 00:33:36 -0500

    still sort of working, checking math

--- middlelayer/halfLifeStats.php
+++ middlelayer/halfLifeStats.php
@@ -4,66 +4,68 @@
 	error_reporting(E_ALL);
 
 	$dbo = new PDO('mysql:host='.$dbhostaddress.';dbname='.$dbname, $dbuser, $dbpass);
-	$gett = $dbo->prepare("SELECT cupnumber,time FROM cups WHERE `time` BETWEEN (:time) - INTERVAL 6 HOUR AND (:time)");//SELECT '2008-02-31' + INTERVAL 0 DAY;
+	$gett = $dbo->prepare("SELECT cupnumber,time FROM cups WHERE `time` BETWEEN (:time) - INTERVAL 12 HOUR AND (:time)");//SELECT '2008-02-31' + INTERVAL 0 DAY;
 	$gett->execute(array(':time' => date('Y-m-d H:i:s')));
 	$done= $gett->fetchAll();
 	//print_r($done);
 
 	$arrayCount = count($done);
 	//echo $arrayCount;
+	$oldDateTime;
 	$i = 0;
 	do {
 	    //echo "<br />".$done[$i]['time'];
+
 	    
 	    //echo slopeIntercept($done[$i]['time'],$i,$arrayCount);
-		$interval = timeDiff($done[$i]['time'],$i);
-		if($i>0 && $arrayCount >= $i){
+		$interval = timeDiff($done[$i]['time'],$done[$i+1]['time']);
+		if($arrayCount===$i){
+
+		}
+		elseif($i>0){
+
+			if($i===1){
+				$degradedAnsw = 1- ((1/(6*60*60))*$interval);
+			}
 			$arrayString .= ",['".$done[$i]['time']."', ".$degradedAnsw."]";
 			
-			$newAnsw = ((1/(6*60*60))*$interval);//add the new cup of coffee i.e. 1
+			$newAnsw = 1+((1/(6*60*60))*$interval);//add the new cup of coffee i.e. 1
 			if($newAnsw>2){
 				$newAnsw =0;
 			}
 			//print new ANSW
 			$arrayString .= ",['".$done[$i]['time']."', ".$newAnsw."]";
 
-			$degradedAnsw = ((1/(6*60*60))*$interval) - $newAnsw;
+			$degradedAnsw = $newAnsw - ((1/(6*60*60))*$interval);
 		}
 		elseif($i===0) {
+			//echo $interval;
 			//start creating the Google array, and add the first time with a value of 1
-			$degradedAnsw = 1 - (1/(6*60*60))*$interval;
+			
 			$arrayString = "[['time', 'coffeelevel'],['".$done[$i]['time']."', 1]";
+			$oldDateTime = $done[$i]['time'];
+		}
 
-		}
-		if($arrayCount <= $i){
-			//done, wrap up
-			$arrayString .= "]";
-			echo $arrayString;
-		}
+
 	    $i++;
 
 	} while (!empty($done[$i]));
-	echo $arrayString;
+	echo $arrayString."]";
 
 
 
-	function timeDiff($thisDateTime,$i){
-		if($i>0){
-			//$timeDiff = abs(strtotime($dateTime) - strtotime($oldDateTime));
-			$first_date = new DateTime($oldDateTime);
-			$second_date = new DateTime($thisDateTime);
+	function timeDiff($thisDateTime,$nextDateTime){
+
+			$first_date = new DateTime($thisDateTime);
+			$second_date = new DateTime($nextDateTime);
 
 			$timeDiff = $first_date->diff($second_date);
-			$oldDateTime = $thisDateTime;
 			
-			$timeAdder = (($timeDiff->h)*(60*60)) + (($timeDiff->m)*60) + $timeDiff->s;
-			echo $timeAdder.'<br />';
+			
+			$timeAdder = (($timeDiff->h)*(60*60)) + (($timeDiff->m)*(60)) + $timeDiff->s;
+			echo $thisDateTime.'<br />'.$nextDateTime.'<br />'.$timeAdder.'<br />'.$timeDiff->h.'<br />'. $timeDiff->m .'<br />'.$timeDiff->s.'<br />';
+
 			return $timeAdder;
-		}
-		elseif($i===0) {
-			$oldDateTime = $thisDateTime;
-			//return "elsemet";
-		}
 	}
 
 

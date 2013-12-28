381f9cdd192e0fe9ddd46a1e952eaf1958cb9462
Jason Hejna <jason.hejna@gmail.com>
2013-12-28 11:53:40 -0500

    almost working

--- middlelayer/halfLifeStats.php
+++ middlelayer/halfLifeStats.php
@@ -10,42 +10,39 @@
 	//print_r($done);
 
 	$arrayCount = count($done);
-	//echo $arrayCount;
+	echo $arrayCount.'<br/>';
 	$i = 0;
 	do {
-		if($i < $arrayCount -1){
+		if($i<$arrayCount -1){
 			$interval = timeDiff($done[$i]['time'],$done[$i+1]['time']);
 		}
-		if($interval > (6*60*60)){
-			$arrayString .= ",['".$done[$i]['time']."', 0]";
-			$interval = 0;
-		}
-		if($i>0 && $interval <= (6*60*60)){
+		if($i>0){
 
 			if($i===1){
 				$degradedAnsw = 1-((1/(6*60*60))*$interval);
 			}
-			if($i>=$arrayCount -1){
+			if($i===$arrayCount){
 				$durationseconds = $degradedAnsw*(6*60*60);
-				$dateinsec=strtotime($done[$i]['time']);
+				echo $i.'<br />';
+				$dateinsec=strtotime($done[$i-1]['time']);
+				echo $done[$i]['time'].'<br />';
 				$newdate=$dateinsec+$durationseconds;
 				$newdate = date('Y-m-d H:i:s',$newdate);
 				$degradedAnsw=0;
 				$arrayString .= ",['".$newdate."', ".$degradedAnsw."]";
 			}
-			else{
 
 			$arrayString .= ",['".$done[$i]['time']."', ".$degradedAnsw."]";
 			
 			$newAnsw = 1+$degradedAnsw;//add the new cup of coffee i.e. 1
 			if($degradedAnsw>2){
 				$degradedAnsw =0;
-				echo "greater2";
+				echo "greater2<br />";
 			}
 			$arrayString .= ",['".$done[$i]['time']."', ".$newAnsw."]";
 
-			$degradedAnsw = $newAnsw - (1/(6*60*60))*$interval;
-			}
+			$degradedAnsw = $newAnsw - ((1/(6*60*60))*$interval);
+
 		}
 		elseif($i===0) {
 			$arrayString = "[['time', 'coffeelevel'],['".$done[$i]['time']."', 1]";
@@ -53,7 +50,8 @@
 
 	    $i++;
 
-	} while (!empty($done[$i]));	
+	} while ($i<=$arrayCount);
+
 	echo $arrayString."]";
 
 	function timeDiff($thisDateTime,$nextDateTime){
